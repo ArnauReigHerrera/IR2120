193ab5b472be70ecaba789742ad70eaa3e691304
ArnauReigHerrera <113519981+ArnauReigHerrera@users.noreply.github.com>
2023-10-19 17:48:21 +0200

    Add files via upload

--- scripts/pick-place-01.py
+++ scripts/pick-place-01.py
@@ -24,30 +24,31 @@ observation_pose = PoseObject(
 
 # Place pose
 place_pose = PoseObject(
-   x=0.0, y=-0.2, z=0.12,
+   x=0.0, y=-0.2, z=0.1,
    roll=0.0, pitch=1.57, yaw=-1.57
 )
 
 
 # - Initialization
 
-
 # Connect to robot
 # Calibrate robot if the robot needs calibration
 robot.update_tool()
 
-
 # Initializing variables
 offset_size = 0.05
-max_catch_count = 4
+catch_count = 0
+max_catch_count = 6
 
+
+offset_x = {ObjectColor.GREEN:0.0, ObjectColor.BLUE:0.03, ObjectColor.RED:-0.03}
+cont_col = {ObjectColor.GREEN:0, ObjectColor.BLUE:0, ObjectColor.RED:0}
 
 # Loop until enough objects have been caught
-catch_count = 0
+
 while catch_count < max_catch_count:
    # Moving to observation pose
    robot.move_pose(observation_pose)
-
 
    # Trying to get object via Vision Pick
    obj_found, shape, color = robot.vision_pick(workspace_name)
@@ -57,13 +58,11 @@ while catch_count < max_catch_count:
 
 
    # Calculate place pose and going to place the object
-   next_place_pose = place_pose.copy_with_offsets(x_offset=catch_count * offset_size)
+   next_place_pose = place_pose.copy_with_offsets(x_offset=offset_x[color], y_offset=cont_col[color]*-0.03)
    robot.place_from_pose(next_place_pose)
 
-
+   cont_col[color] += 1
    catch_count += 1
-
 
 robot.go_to_sleep()
 robot.close_connection()
-
